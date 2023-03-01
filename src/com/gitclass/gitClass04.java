1b00b97b689c21292076caa57dc0ff97dc64d28e
erickLop503 <4everklopez@gmail.com>
2023-03-01 00:28:03 -0500

    adding git class

--- src/com/gitclass/gitClass04.java
+++ src/com/gitclass/gitClass04.java
@@ -2,4 +2,10 @@ package com.gitclass;
 
 public class gitClass04 {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Anda pasha bobo!");
+		
+		System.out.println("Dale campeon Argentina!!!");
+	}
 }
